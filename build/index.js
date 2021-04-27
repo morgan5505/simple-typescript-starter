5e0a20542763e30b20af68209a679a88b7e81fa9
Kevin Morgan <morgan5505@gmail.com>
2021-04-27 15:50:39 -0400

    wip

--- build/index.js
+++ build/index.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log('Hello world!');
+var sampleString = "(Name)John Doe\n(Age)20\n(City)Ashtabula, OH\n(Flags)NYN\n\n(Name)Jane Doe\n(Flags)YNY\n(City)N Kingsville, OH\n\n(Name)Sally Jones\n(Age)25\n(City)Paris\n(Flags)YYY\n";
+var stringArray = sampleString.split('\n');
+console.log(stringArray);
+var output = [];
+stringArray.map(function (item) {
+    var current = 0;
+    if (item.includes('(Name)')) {
+        console.log(item);
+        //     console.log(item);
+        var name = item.replace('(Name)', '');
+        //     console.log(name);
+        output[current] = {
+            name: name,
+        };
+        //     current += 1;
+        //   }
+    }
+    if (item === '') {
+        current += 1;
+    }
+});
+console.log(output);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,66 @@
-console.log('Hello world!');
+const sampleString = `(Name)John Doe
+(Age)20
+(City)Ashtabula, OH
+(Flags)NYN
+
+(Name)Jane Doe
+(Flags)YNY
+(City)N Kingsville, OH
+
+(Name)Sally Jones
+(Age)25
+(City)Paris
+(Flags)YYY
+`;
+
+const stringArray = sampleString.split('\n');
+
+const output: {
+  name: string;
+  age?: string;
+  city?: string;
+  state?: string;
+  gender?: string;
+  student?: string;
+  employee?: string;
+}[] = [];
+let current = 0;
+stringArray.map(item => {
+  if (item.includes('(Name)')) {
+    const name = item.replace('(Name)', '');
+    output[current] = {
+      name,
+    };
+  } else if (item.includes('Age')) {
+    const age = item.replace('(Age)', '');
+    const person = output[current];
+    person.age = age;
+    output[current] = person;
+  } else if (item.includes('City')) {
+    const cityState = item.replace('(City)', '');
+    const person = output[current];
+    person.city = cityState;
+    output[current] = person;
+  } else if (item.includes('Flags')) {
+    const flags = item.replace('(Flags)', '').split('');
+    const person = output[current];
+    const gender = flags[0] === 'Y' ? 'Female' : 'Male';
+    const isStudent = flags[1] === 'Y' ? 'Yes' : 'No';
+    const isEmployee = flags[2] === 'Y' ? 'Yes' : 'No';
+    person.gender = gender;
+    person.student = isStudent;
+    person.employee = isEmployee;
+    output[current] = person;
+  }
+  if (item === '') {
+    current += 1;
+  }
+});
+
+output.map(({ name, age, city, gender, student, employee, state }) => {
+  console.log(`${name} [${age}, ${gender}]
+  \tCity: ${city}
+  \tState: ${state}
+  \tStudent: ${student}
+  \tEmployee: ${employee}`);
+});
